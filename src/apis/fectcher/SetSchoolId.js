f42a1e9d8be8e62382d954725053b4f8370e38df
adeshshrma <me@mydomail.com>
2023-01-27 17:14:07 +0530

    added auth header

--- src/apis/fectcher/SetSchoolId.js
+++ src/apis/fectcher/SetSchoolId.js
@@ -1,9 +1,14 @@
-import axios from "axios";
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import instance from '../../instance'
 
 export const setSchoolId = async (id) => {
-  const res = await axios
-    .get(
-      `https://schoolsbcontent.imaxprogram.com/app/schoolApp/selectedSchool/?schoolAcademicYearId=8247`
-    )
-    .catch((err) => console.log(err));
-};
+  console.log(Cookies.get('token'))
+  const res = await instance({
+    url: 'schoolApp/selectedSchool/?schoolAcademicYearId=8247',
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${Cookies.get('token')}`,
+    },
+  }).catch((err) => console.log(err))
+}
--- src/apis/fectcher/assessment/overview/overviewData.js
+++ src/apis/fectcher/assessment/overview/overviewData.js
@@ -1,17 +1,21 @@
 // import axios from "axios";
-import instance from "../../../../instance";
+import Cookies from 'js-cookie'
+import instance from '../../../../instance'
 
 export const GetOverviewData = async (id) => {
   const res = await instance({
     url: `schoolApp/configuration/overviewData/${id}`,
-    method: "GET",
-  }).catch((err) => console.log(err));
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${Cookies.get('token')}`,
+    },
+  }).catch((err) => console.log(err))
   // const res = await axios
   //   .get(
   //     `https://schoolsbcontent.imaxprogram.com/app/schoolApp/configuration/overviewData/${id}`
   //   )
   //   .catch((err) => console.log(err));
   // console.log(res.data);
-  const result = res.data.examOverviewData;
-  return result;
-};
+  const result = res.data.examOverviewData
+  return result
+}

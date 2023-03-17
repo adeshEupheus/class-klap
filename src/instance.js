7065f1ece85e78efdc3851ba3d40ce72fad455c2
adesh sharma <adeshs@eupheus.in>
2023-03-17 14:19:05 +0530

    added stage url

--- src/instance.js
+++ src/instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://school.classklap.com/app/",
+  // baseURL: "https://school.classklap.com/app/",
+  baseURL: "https://schoolsbsm.xamcheck.com/app/",
 });
 
 export default instance;

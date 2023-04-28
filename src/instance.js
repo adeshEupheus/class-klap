7b49eb9b62001c8e4476de079b95a4afda4e65dd
adesh sharma <adeshs@eupheus.in>
2023-04-28 14:49:55 +0530

    instance url changed

--- src/instance.js
+++ src/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://school.classklap.com/app/",
-  baseURL: "https://schoolsbel.xamcheck.com/app/",
+  baseURL: "https://school.classklap.com/app/",
+  // baseURL: "https://schoolsbel.xamcheck.com/app/",
 });
 
 export default instance;
